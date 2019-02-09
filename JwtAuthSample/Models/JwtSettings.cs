4c10d945740699d3d86c8b038f4e656d37dece49
Tom <18158510+Ran-snow@users.noreply.github.com>
2019-02-09 23:07:10 +0800

    JWT学习

---
+++ JwtAuthSample/Models/JwtSettings.cs
@@ -0,0 +1,9 @@
+namespace JwtAuthSample
+{
+    public class  JwtSettings
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string SecretKey { get; set; }
+    }
+}
