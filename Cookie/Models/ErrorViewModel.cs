9c0f76a5b6663ed99b5b56e7cd659ff66f816022
Tom <18158510+Ran-snow@users.noreply.github.com>
2019-05-09 06:07:28 +0800

    cookie

---
+++ Cookie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cookie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
