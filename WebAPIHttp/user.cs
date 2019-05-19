9fd1a39e2c05a4bf251d4740b067ed5d4fb34ea1
Tom <18158510+Ran-snow@users.noreply.github.com>
2019-05-19 20:45:13 +0800

    Webapi 接收参数，返回对象 学习

---
+++ WebAPIHttp/user.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIHttp
+{
+    /// <summary>
+    /// User
+    /// </summary>
+    /* {
+	"name":"test",
+	"age":16,
+	"gander":"female"
+    } */
+    public class user
+    {
+        public string name { get; set; }
+        public string age { get; set; }
+        public string gander { get; set; }
+    }
+}
