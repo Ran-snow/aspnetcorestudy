01eb42ad8419f11dc1e530c5b0502ca57336f4dc
Tom <18158510+Ran-snow@users.noreply.github.com>
2019-06-20 16:26:02 +0800

    add webapi

--- Cookie/Program.cs
+++ Cookie/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -20,5 +23,40 @@ namespace Cookie
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        public static List<string> GetActiveMacAddress(string separator = "-")
+        {
+            var macAddress = new List<string>();
+
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+
+            if (nics == null || nics.Length < 1)
+            {
+                Console.WriteLine("  No network interfaces found.");
+                return macAddress;
+            }
+
+            foreach (NetworkInterface adapter in nics.Where(c =>
+                c.NetworkInterfaceType != NetworkInterfaceType.Loopback && c.OperationalStatus == OperationalStatus.Up))
+            {
+                IPInterfaceProperties properties = adapter.GetIPProperties();
+
+                var unicastAddresses = properties.UnicastAddresses;
+                if (unicastAddresses.Any(temp => temp.Address.AddressFamily == AddressFamily.InterNetwork))
+                {
+                    var address = adapter.GetPhysicalAddress();
+                    if (string.IsNullOrEmpty(separator))
+                    {
+                        macAddress.Add(address.ToString());
+                    }
+                    else
+                    {
+                        macAddress.Add(string.Join(separator, address.GetAddressBytes()));
+                    }
+                }
+            }
+
+            return macAddress;
+        }
     }
 }
