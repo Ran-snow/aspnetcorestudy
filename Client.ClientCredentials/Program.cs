17da79bc6c0a30c3797f90e2bfc61126c962cf01
Tom <18158510+Ran-snow@users.noreply.github.com>
2019-07-18 23:40:15 +0800

    OAuth 客户端模式

---
+++ Client.ClientCredentials/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net.Http;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace Client.ClientCredentials
+{
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// Install-Package IdentityModel / dotnet add package IdentityModel
+        /// </remarks>
+        /// <param name="args"></param>
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Console.WriteLine("ClientCredentials");
+
+            // discover endpoints from metadata
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "client",
+                ClientSecret = "511536EF-F270-4058-80CA-1C89C192F69A",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+
+            // call api
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/api/values");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
