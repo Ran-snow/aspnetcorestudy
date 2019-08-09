da23531a4670a1a204ee983b043df1b34030e713
Tom <18158510+Ran-snow@users.noreply.github.com>
2019-08-09 11:22:19 +0800

    Databind

---
+++ WebAPIDataBind/Controllers/ATagsController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIDataBind.Model;
+
+namespace WebAPIDataBind.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ATagsController : ControllerBase
+    {
+        [HttpPost]
+        [HttpGet]
+        public object FromSimple(string name)
+        {
+            return "res "+name;
+        }
+
+        [HttpPost]
+        [HttpGet]
+        public object FromSimpleFrom([FromForm]string name)
+        {
+            return "res "+name;
+        }
+
+        [HttpPost]
+        [HttpGet]
+        public object RawSimple(string name)
+        {
+            return "res "+name;
+        }
+
+        [HttpPost]
+        public object RawSimpleFromBody([FromBody]string name)
+        {
+            return "res "+name;
+        }
+
+        [HttpGet]
+        public object RawSimpleFromQuery([FromBody]string name)
+        {
+            return "res "+name;
+        }
+
+        //--------------
+
+        [HttpPost]
+        [HttpGet]
+        public object FromComplex(User user)
+        {
+            return "res "+user;
+        }
+
+        [HttpPost]
+        [HttpGet]
+        public object FromComplexFrom([FromForm]User user)
+        {
+            return "res "+user;
+        }
+
+        [HttpPost]
+        [HttpGet]
+        public object RawComplex(User user)
+        {
+            return "res "+user;
+        }
+
+        [HttpPost]
+        public object RawComplexFromBody([FromBody]User user)
+        {
+            return "res "+user;
+        }
+
+        [HttpGet]
+        public object RawComplexFromQuery([FromQuery]User user)
+        {
+            return "res "+user;
+        }
+    }
+}
---
+++ WebAPIDataBind/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIDataBind.Model
+{
+    public class User
+    {
+        public string name { get; set; }
+
+        public int age { get; set; }
+    }
+}
