04b48d8135a4691a51e78dd31bed1671e39a22ce
Tom <18158510+Ran-snow@users.noreply.github.com>
2020-01-11 10:53:03 +0800

    SemaphoreSlimLearn

---
+++ CharpLearn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CharpLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CharpLearn/SemaphoreSlimLearn.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CharpLearn
+{
+    public class SemaphoreSlimLearn
+    {
+        private static SemaphoreSlim semaphore;
+        // A padding interval to make the output more orderly.
+        private static int padding;
+
+        public static void Main()
+        {
+            // Create the semaphore.
+            semaphore = new SemaphoreSlim(0, 3);
+            Console.WriteLine("{0} tasks can enter the semaphore.",
+                              semaphore.CurrentCount);
+            Task[] tasks = new Task[5];
+
+            // Create and start five numbered tasks.
+            for (int i = 0; i <= 4; i++)
+            {
+                tasks[i] = Task.Run(() =>
+                {
+                    // Each task begins by requesting the semaphore.
+                    Console.WriteLine("Task {0} begins and waits for the semaphore.",
+                                      Task.CurrentId);
+                    semaphore.Wait();
+
+                    Interlocked.Add(ref padding, 100);
+
+                    Console.WriteLine("Task {0} enters the semaphore.", Task.CurrentId);
+
+                    // The task just sleeps for 1+ seconds.
+                    Thread.Sleep(1000 + padding);
+
+                    Console.WriteLine("Task {0} releases the semaphore; previous count: {1}.",
+                                      Task.CurrentId, semaphore.Release());
+                });
+            }
+
+            // Wait for half a second, to allow all the tasks to start and block.
+            Thread.Sleep(500);
+
+            // Restore the semaphore count to its maximum value.
+            Console.Write("Main thread calls Release(3) --> ");
+            semaphore.Release(3);
+            Console.WriteLine("{0} tasks can enter the semaphore.",
+                              semaphore.CurrentCount);
+            // Main thread waits for the tasks to complete.
+            Task.WaitAll(tasks);
+
+            Console.WriteLine("Main thread exits.");
+        }
+    }
+}
